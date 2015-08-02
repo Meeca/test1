c946a2a238144d990ea04af23e39fb16241ce15e
MiniC <miniccode@163.com>
2015-08-02 10:42:09 +0800

    minic
    我的第一个测试程序

---
+++ Test1/Test1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test1
+//
+//  Created by 冯洪建 on 15/8/2.
+//  Copyright (c) 2015年 冯洪建. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test1/Test1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test1
+//
+//  Created by 冯洪建 on 15/8/2.
+//  Copyright (c) 2015年 冯洪建. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
